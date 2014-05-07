d5da694ec91f01031b103c4c4451fc4790657672
lenin <leninkhaidem@gmail.com>
2014-05-07 16:13:56 +0530

    3rd commit

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('maskofzorrod'));
-app.use(express.session());
+//app.use(express.cookieParser('maskofzorrod'));
+//app.use(express.session());
 app.use(app.router);
 app.use(express.compress());
 app.use(express.static(path.join(__dirname, 'bower_components')));
@@ -42,43 +42,74 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', function(req, res) {
-    if (req.cookies.search) {
-        res.clearCookie('search');
-    }
+    //if (req.cookies.search) {
+    //res.clearCookie('search');
+    //}
     res.render('pic');
 });
 app.post('/search', function(req, res) {
     var string = req.body.search;
-    console.dir(req.cookies);
     console.log('got a request! current search string is ' + string + ' cached string is ' + req.cookies);
-    if (!req.cookies.search || (req.cookies.search != string)) {
-        console.log('not search ' + string + ' before');
-        var link = "http://www.imagesbazaar.com/advancesearchresult.aspx?id=" + string + "%20&idtot=" + string + "&exec=True&nonexec=True";
-        jsdom.env({
-            url: link,
-            src: [jquery],
-            done: function(errors, window) {
-                if (errors) {
-                    console.log("error occured while trying to download");
-                    res.json([]);
-                } else {
-                    var count = 0;
-                    output = [];
-                    /*output = {
+    //if (!req.cookies.search || (req.cookies.search != string)) {
+    console.log('not search ' + string + ' before');
+    var link = "http://www.imagesbazaar.com/advancesearchresult.aspx?id=" + string + "%20&idtot=" + string + "&exec=True&nonexec=True";
+    jsdom.env({
+        url: link,
+        src: [jquery],
+        done: function(errors, window) {
+            if (errors) {
+                console.log("error occured while trying to download");
+                res.json([]);
+            } else {
+                var count = 0;
+                var map = {};
+
+                output = [];
+                /*output = {
                         length: 0,
                         url: {}
                     };*/
-                    var $ = window.$;
-                    res.cookie('search', string);
-                    $("img").each(function() {
-                        if ($(this).attr('src').match(/http.*\.jpg/)) {
-                            count++;
-                            output.push($(this).attr('src'));
-                            /*output.url[count] = $(this).attr('src');*/
+                var $ = window.$;
+                //res.cookie('search', string);
+                $(".box table td").each(function() {
+                    var gid = 0,
+                        oid = 0
+                        id = 0;
+                    var that = $(this);
+                    that.find("a").each(function() {
+                        var link = $(this).attr('href');
+                        if (link.match(/preview/)) {
+                            var myRegEx = /.*gid=([0-9]+)/g;
+                            var matches = getMatches(link, myRegEx, [1]);
+                            gid = matches[0];
                         }
                     });
+                    that.find("img").each(function() {
+                        if ($(this).attr('src').match(/http/)) {
+                            id = $(this).attr('id');
 
-                    if (output.length == 0) {
+                        }
+                        //console.log(id);
+                    });
+                    map[id] = gid;
+                    /*if ($(this).attr('src').match(/http.*\.jpg/)) {
+                            count++;
+                            output.push($(this).attr('src'));
+                            //output.url[count] = $(this).attr('src');
+                        }*/
+                });
+                for (id in map) {
+                    //console.log(id + " " + map[id]);
+                    if (id.match(/.*\-.*/)) {
+                        var matches = getMatches(id, /(?:.*)\-(.*)/g, [1]);
+                        oid = matches[0];
+                        gid = map[id];
+                        output.push('http://www.imagesbazaar.com/WatermarkCopyright.aspx?oderid=' + oid + '&group=' + gid + '&name=' + id + '.jpg&size=900');
+                    }
+
+                }
+                res.json(output);
+                /*if (output.length == 0) {
                         res.json([]);
                     } else if (output.length > 0 && output.length <= 10) {
                         res.json(output);
@@ -90,12 +121,12 @@ app.post('/search', function(req, res) {
                         };
                         console.log(temp);
                         res.json(temp);
-                    }
-                }
+                    }*/
             }
-        });
-    } else {
-        if (output.length == 0) {
+        }
+    });
+    //} else {
+    /*if (output.length == 0) {
             res.json([]);
         } else if (output.length > 0 && output.length <= 10) {
             res.json(output);
@@ -106,8 +137,9 @@ app.post('/search', function(req, res) {
                 temp.push(output.pop());
             };
             res.json(temp);
-        }
-    }
+        }*/
+    // res.json(output);
+    //}
 
 });
 
@@ -123,3 +155,19 @@ https.createServer({
 http.createServer(app).listen(80, function() {
     console.log('Express server listening on port 80');
 });
+
+function getMatches(string, regex, index) {
+    index || (index = [1]); // default to the first capturing group
+    var matches = [];
+    var match;
+    /*
+    while (match = regex.exec(string)) {
+        matches.push(match[index]);
+    }*/
+
+    match = regex.exec(string);
+    for (var i = 0; i < index.length; i++) {
+        matches.push(match[index[i]]);
+    };
+    return matches;
+}
--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
+    var url = []
     var count = 0;
+    /*
     $(document).scroll(function(event) {
-        /* Act on the event */
 
 
         if ($(window).height() + $(window).scrollTop() == $(document).height()) {
-            $("#submitButton").trigger('click');
+            //$("#submitButton").trigger('click');
             count++;
             console.log(count + ' hit the bottom');
         }
+    });*/
+    $("#searchAgain").click(function() {
+        location.reload();
+
     });
     $("#submitButton").click(function() {
-
-        var htmlContent = "";
+        $("#area1").hide(1000);
+        $('#area2').hide().addClass('circle').fadeIn(3000);
         if ($("#searchString").val().length) {
-
-
             $.ajax({
                 url: '/search',
                 type: 'post',
@@ -30,18 +33,44 @@ $(document).ready(function() {
                         alert('search returned 0 result');
                     } else {
                         for (var i = 0; i < data.length; i++) {
-                            htmlContent += '<li><a href="#pic' + i + '"><h1>Picture' + i + '</h1><img class="lazy" data-original="' + data[i] + '"></a></li>';
+                            data[i] = '<li><a href="#pic' + i + '"><img class="img" data-src="' + data[i] + '"></a><h1 class="center-wrapper">Picture' + i + '</h1></li>';
+                            //htmlContent += '<li><a href="#pic' + i + '"><h1>Picture' + i + '</h1><img class="img" data-src="' + data[i] + '"></a></li>';
                         };
-                        $('#imageList').append(htmlContent);
+                        url = data;
+                        $('#imageList').html(url.pop());
+                        $.extend($.lazyLoadXT, {
+                            edgeY: 0,
+                            onshow: {
+                                addClass: 'circle'
+                            },
+                            onload: {
+                                removeClass: 'circle'
+                            }
+                        });
+                        // $.lazyLoadXT.autoInit = false;
+                        $('img').lazyLoadXT();
+                        $("#area1").fadeIn(1500);
+                        $('#area2').removeClass('circle');
+                        $('#searchArea').fadeOut(500);
+                        $('#marker-end').on('lazyshow', function() {
+                            $('#imageList').append(
+                                url.pop()
+                            );
+                            $(window).lazyLoadXT();
+                            $('#marker-end').lazyLoadXT({
+                                visibleOnly: false,
+                                checkDuplicates: false
+                            });
+                        }).lazyLoadXT({
+                            visibleOnly: false
+                        });
                     }
-                    $(function() {
-                        $("img.lazy").lazyload();
-                    });
                 })
                 .fail(function() {
+                    $("#area1").fadeIn(1500);
+                    $('#area2').removeClass('circle');
                     alert("error");
                 })
         }
     });
-
 });
